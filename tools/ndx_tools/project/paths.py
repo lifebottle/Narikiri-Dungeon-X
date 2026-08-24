95514950a5ce3787b2e798f6dfeda5ec1c0019ea
Stewie <74146408+fortiersteven@users.noreply.github.com>
2026-08-24 15:35:03 -0400

    Tool port rebirth split section (#86)
    * Replace MenuFiles with updated version from Pegi
    
    * Extract Eboot
    
    * Add new XMLs

--- tools/ndx_tools/project/paths.py
+++ tools/ndx_tools/project/paths.py
@@ -1,6 +1,8 @@
 import shutil
 from pathlib import Path
 
+from pygments.lexers import json5
+import pyjson5 as json
 from loguru import logger
 
 default_iso: Path = Path("ndx.iso")
@@ -9,11 +11,13 @@ original_eboot: Path = Path("0_disc/PSP_GAME/SYSDIR/EBOOT.BIN")
 all_dat: Path = Path("0_disc/PSP_GAME/USRDIR/all.dat")
 extracted_files: Path = Path("1_extracted")
 decrypted_eboot: Path = Path("1_extracted/EBOOT.BIN")
+original_files: Path = Path("1.5_original")
 translation_files: Path = Path("2_translated")
 patched_files: Path = Path("3_patched")
 game_builds: Path = Path("4_builds")
 binaries: Path = Path("tools/bin")
 hashes: Path = Path("project/hashes.json")
+menu_json: Path = Path("project/MenuFiles.json")
 
 def clean_folder(path: Path) -> None:
     target_files = list(path.iterdir())
@@ -37,3 +41,10 @@ def clean_builds(path: Path) -> None:
     for file in target_files:
         logger.info(f"deleting {str(file.name)}...")
         file.unlink()
+
+
+def read_menu_json():
+    with menu_json.open("r", encoding="utf-8") as f:
+        data = json.load(f)
+
+        return { ele['friendly_name']:ele for ele in data }
