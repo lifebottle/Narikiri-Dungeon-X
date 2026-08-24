5e3249fd00e35d11b565c816ef329c870d19a626
fortiersteven <fortiersteven1@gmail.com>
2026-08-24 16:21:59 -0400

    Add code to extract other menus
    Adjust code

--- tools/ndx_tools/formats/menu.py
+++ tools/ndx_tools/formats/menu.py
@@ -3,7 +3,7 @@ from ndx_tools.utils.fileio import FileIO
 from ndx_tools.formats.xml import TlText
 from ndx_tools.utils.string import bytes_to_text
 from dataclasses import dataclass, field
-from typing import Any
+from typing import Any, List, Tuple
 
 from pathlib import Path
 from typing import Self, cast
@@ -35,16 +35,22 @@ class Menu:
         self.file_path = file_path
         self.base_offset = entry['base_offset']
         self.friendly_name = entry['friendly_name']
+        self.split_sections = entry['split_sections']
         self.menu_sections = [
             MenuSection.from_dict(section)
             for section in entry.get("sections", [])
         ]
+        self.entry_sections: List[Tuple[str, List]] = []
         self.id = 1
 
     @staticmethod
     def fetch_clean_section_name(section:str):
         first_occ = section.find("[")
-        return section[:first_occ-1]
+
+        if first_occ > 0:
+            return section[:first_occ-1]
+        else:
+            return section
 
     @staticmethod
     def fetch_text_area(name:str):
@@ -63,7 +69,7 @@ class Menu:
 
         # Collect the canonical pointer for the embedded pairs
         # might need a function extract here?
-        self.entry_sections = []
+        #self.entry_sections = []
 
         with FileIO(self.file_path, 'rb') as f:
             for section in self.menu_sections:
@@ -76,53 +82,61 @@ class Menu:
     def get_style_pointers(self, f:FileIO, ptr_range: tuple[int, int], style: str):
 
         if style == "*":
-            pointers_offset, pointers_value = self.get_all_possible(f, ptr_range)
+            return self.get_all_possible(f, ptr_range)
 
         else:
-            pointers_offset, pointers_value = self.get_regular_pattern(f, ptr_range, style)
-
-        return pointers_offset, pointers_value
+            return self.get_regular_pattern(f, ptr_range, style)
 
 
 
     def extract_section_basic(self, section: MenuSection, f:FileIO) -> None:
 
-        # Extract Pointers list out of the file
-        pointers_offset = []
-        pointers_value = []
-
-        pointers_offset_, pointers_value_ = self.get_style_pointers(f, (section.pointers_start, section.pointers_end),
+        # Extract Pointers list out of the file,
+        pointers_offset, pointers_value, mlen = self.get_style_pointers(f, (section.pointers_start, section.pointers_end),
                                                                     section.style)
-        pointers_offset.extend(pointers_offset_)
-        pointers_value.extend(pointers_value_)
+        if len(mlen) == 0:
+            mlen = [0] * len(pointers_offset)
 
         # Make a list, we also merge the emb pointers with the
         # other kind in the case they point to the same text
         entries = []
         temp = dict()
         f.seek(0)
-        for off, val in zip(pointers_offset, pointers_value):
+        for off, val, max_len in zip(pointers_offset, pointers_value, mlen):
             text = bytes_to_text(f, offset=val)
             temp.setdefault(text, dict()).setdefault("ptr", []).append(off)
-            entries.append( (off, text, self.id))
+            entries.append( (off, text, self.id, max_len))
             self.id = self.id + 1
 
+        #if section.style[0] == "T":
+        #    max_len = int(section.style[1:])
 
         # Remove duplicates
         #list_informations = [(k, str(v['ptr'])[1:-1], v.setdefault('emb', None)) for k, v in temp.items()]
         self.entry_sections.append((section.name, entries))
 
+        t = 2
+
     def make_xml(self, folder: Path) -> None:
 
+        xml = TlXml()
         for section, entries in (pb := tqdm(self.entry_sections)):
-            xml = TlXml()
+
             pb.set_description(f'menu/{section}')
 
-            for off, text, id in entries:
-                xml.add_text(section, text, off)
+            for off, text, id, mlen in entries:
+                xml.add_text(section, text, off, max_len=mlen)
 
-            name = f'Eboot - {section}.xml'
+            if self.split_sections:
+                name = f'{self.friendly_name} - {section}.xml'
+                xml.save_xml(folder / name)
+                xml = TlXml()
+
+        if not self.split_sections:
+            name = f'{self.friendly_name}.xml'
             xml.save_xml(folder / name)
+
+
 
     def get_all_possible(self, f:FileIO, ptr_range: tuple[int, int]):
         pointers_offset: list[int] = []
@@ -140,16 +154,17 @@ class Menu:
                     pointers_offset.append(f.tell() - 4)
                     pointers_value.append(file_offset)
 
-        return pointers_offset, pointers_value
+        return pointers_offset, pointers_value, [0] * len(pointers_offset)
 
     def get_regular_pattern(self, f:FileIO, ptr_range: tuple[int, int], style: str):
         split: list[str] = [ele for ele in re.split(r'([PT])|(\d+)', style) if ele]
         pointers_offset: list[int] = []
         pointers_value: list[int] = []
+        mlen: list[int] = []
         f.seek(ptr_range[0])
 
         while f.tell() < ptr_range[1]:
-            for step in split:
+            for i, step in enumerate(split):
 
                 if step == "P":
                     off = f.read_uint32()
@@ -163,10 +178,11 @@ class Menu:
                     off = f.tell()
                     pointers_offset.append(off)
                     pointers_value.append(off)
+                    mlen.append(int(split[i+1]))
                 else:
                     f.read(int(step))
 
-        return pointers_offset, pointers_value
+        return pointers_offset, pointers_value, mlen
 
     def pack_file(self, destination_path:Path, xml_path:Path, list_status_insertion):
 
@@ -213,7 +229,7 @@ class Menu:
                 continue
 
             text_bytes = text_to_bytes(entry, 2)
-
+            l = len(text_bytes)
             for pool in pools:
 
                 if l <= pool[1]:
